0ae283d162548063db65012dbebcedfaf94254f2
SHENYUAN <1245869619@qq.com>
2026-01-09 13:31:21 +0800

    考试测评用例调试

--- test_cases/exercise/test_exercise_first.py
+++ test_cases/exercise/test_exercise_first.py
@@ -16,7 +16,7 @@ class TestExerciseCase:
     
     @pytest.mark.asyncio
     async def test_case(self, desktop: DesktopPage, driver):
-        """测试case"""
+        """测试攻防演练模块导航流程"""
         try:
             page = driver.page
             
@@ -28,14 +28,11 @@ class TestExerciseCase:
             await page.get_by_role("menuitem", name="剧本管理").click()
             await expect(page.get_by_role("tab", name="个人剧本")).to_be_visible()
             await page.get_by_role("menuitem", name="活动管理").click()
-            await expect(page.get_by_role("button", name=" 新建演练活动")).to_be_visible()
+            await expect(page.get_by_role("button", name=" 新建演练活动")).to_be_visible()
             await page.get_by_text("内容市场").click()
             await page.get_by_role("menuitem", name="资源安装--").click()
             await expect(page.get_by_text("名称")).to_be_visible()
             await page.locator("span").nth(4).click()
-
-
-
             
         except Exception as e:
             await driver.skip_step(f"测试失败: {e}")
